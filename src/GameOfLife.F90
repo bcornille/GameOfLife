cabd63ba59392062ba1b904232cf0a07411f19ba
bcornille <bcornille7@gmail.com>
2019-07-22 13:49:32 -0500

    Full MPI and hybrid MPI+OpenMP implementation working. OpenMP overhead seems large.

--- src/GameOfLife.F90
+++ src/GameOfLife.F90
@@ -3,34 +3,77 @@ PROGRAM game_of_life
   USE kind_mod
   USE input_mod, ONLY: nsteps
   USE game_board_mod
+#ifdef USE_MPI
+  USE mpi_f08
+#endif
 
   IMPLICIT NONE
 
   TYPE(game_board_type) :: game_board
   INTEGER(ikind_large) :: population, step
 
-#if USE_MPI
-  INTEGER :: mpi_error
-  MPI_Init(mpi_error)
+#ifdef USE_MPI
+  DOUBLE PRECISION :: tstart, total_time
+  INTEGER :: ierror, mpi_thread_provided, rank
+  CALL MPI_Init_thread(MPI_THREAD_MULTIPLE, mpi_thread_provided, ierror)
+  IF(mpi_thread_provided < MPI_THREAD_FUNNELED) THEN
+    CALL MPI_Finalize(ierror)
+    STOP "Not a threaded MPI implementation."
+  ENDIF
+  CALL MPI_Comm_rank(MPI_COMM_WORLD, rank, ierror)
+#else
+  INTEGER(ikind_large) :: count_start, count_end, count_rate
+  REAL(rkind) :: total_time
 #endif
 
   !Start by initializing the game board.
   CALL game_board%initialize()
   population = game_board%count_cells()
   ! CALL game_board%print_state()
-  WRITE(*,*) "Population: ", population
-
+#ifdef USE_MPI
+  IF(rank == 0) THEN
+#endif
+  WRITE(*,'(A12,I8)') "Population: ", population
+#ifdef USE_MPI
+  ENDIF
+  tstart = MPI_Wtime()
+#else
+  CALL SYSTEM_CLOCK(count_start, count_rate)
+#endif
 
   DO step = 1, nsteps
+    CALL game_board%update_halo()
+    ! CALL game_board%print_state()
     CALL game_board%accumulate_neighbors()
     CALL game_board%update_state()
     population = game_board%count_cells()
     ! CALL game_board%print_state()
-    WRITE(*,*) "Population: ", population
+#ifdef USE_MPI
+    IF(rank == 0) THEN
+#endif
+    WRITE(*,'(A12,I8)') "Population: ", population
+#ifdef USE_MPI
+    ENDIF
+#endif
   ENDDO
+#ifdef USE_MPI
+  total_time = MPI_Wtime() - tstart
+#else
+  CALL SYSTEM_CLOCK(count_end)
+  total_time = (count_end - count_start) / REAL(count_rate, rkind)
+#endif
+  CALL game_board%update_halo()
+  ! CALL game_board%print_state()
+#ifdef USE_MPI
+  IF(rank == 0) THEN
+#endif
+  WRITE(*,'(A9,F8.3,A8)') "Runtime: ", total_time, " seconds"
+#ifdef USE_MPI
+  ENDIF
+#endif
 
-#if USE_MPI
-  MPI_Finalize(mpi_error)
+#ifdef USE_MPI
+  CALL MPI_Finalize(ierror)
 #endif
 
 END PROGRAM game_of_life
--- src/kind.F90
+++ src/kind.F90
@@ -2,7 +2,7 @@ MODULE kind_mod
 
   IMPLICIT NONE
 
-  INTEGER, PARAMETER :: ikind = SELECTED_INT_KIND(1),                            &
+  INTEGER, PARAMETER :: ikind = SELECTED_INT_KIND(1),                           &
                         ikind_large = SELECTED_INT_KIND(8),                     &
                         rkind = SELECTED_REAL_KIND(15,1)
 
